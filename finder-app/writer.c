efcc0379f009f07746720e591223d1dc56abdf88
Joel-Ngonga <ngongajoel@gmail.com>
2024-01-25 11:34:16 +0200

    using syscalls

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <syslog.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 
 int main(int argc, char *argv[]){
   if (argc != 3) {// you have entered more or less than 2 arguments
@@ -19,20 +22,27 @@ int main(int argc, char *argv[]){
   syslog(LOG_DEBUG, "writing '%s' to '%s'",filecontent, filename);
   
   //open file for writing
-  FILE *file = fopen(filename, "w");
+  int file = open (filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP |S_IROTH);
   
-  if (file == NULL) {
+  if (file == -1) {
      // LOG unexpected error with LOG_ERR level
+     printf("error file not opened");
      syslog(LOG_ERR, " COULD NOT OPEN FILE");
      closelog();
      exit(EXIT_FAILURE);
      }
   // write content to file
-  fprintf(file, "%s", filecontent);
-      fclose(file);
+  //fprintf(file, "%s", filecontent);
+  ssize_t byteWritten = write(file,filecontent,strlen(filecontent));
+  if (byteWritten == -1){
+     printf("error file not written");
+     exit(EXIT_FAILURE);
+    }
+    close(file);
 
     // Close syslog
     closelog();
+    //printf("all good");
    syslog(LOG_ERR, "all good");
     return EXIT_SUCCESS;
    }
